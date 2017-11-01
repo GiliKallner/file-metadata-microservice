93950d7a9737697149a5cc0bb63f66b42c71214d
Glitch (file-upload-microservice) <none>
2017-11-01 13:33:18 +0000

    file-upload-microservice

--- routes/routes.js
+++ routes/routes.js
@@ -10,10 +10,16 @@ router.get("/", function (request, response) {
   response.sendFile(path.join('/app/views/index.html'));
 });
 
-router.post('/file', upload.single('myfile'), (req, res) =>{
-    
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({size:req.file.size},null,3));
+router.post('/file',(req, res) =>{
+    upload.single('myfile')(req,res,err =>{
+    if(err) throw err;          
+    else if(req.file){          
+          res.setHeader('Content-Type', 'application/json');
+          res.send(JSON.stringify({size:req.file.size},null,3));
+      }
+      else res.send('YOU DIDNT INSERT ANY FILE. PLEASE GO BACK AND TRY AGAIN')
+      res.end();
+    })
 });
 
 module.exports = router;
