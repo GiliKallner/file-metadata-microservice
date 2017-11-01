fcb7b0e60a5fd7c2b380d4fdaa482ee09c69f343
Glitch (file-upload-microservice) <none>
2017-11-01 12:52:14 +0000

    Checkpoint

--- routes/routes.js
+++ routes/routes.js
@@ -10,7 +10,6 @@ router.get("/", function (request, response) {
   response.sendFile(path.join('/app/views/index.html'));
 });
 
- 
 router.post('/file', upload.single('myfile'), function (req, res, next) {
     console.log('body: ',req.body);
     console.log('files: ',req.file);
