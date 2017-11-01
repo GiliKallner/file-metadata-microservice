ecd613c9cff34c3cf501e08e34675cf5421060bc
Glitch (file-upload-microservice) <none>
2017-11-01 11:46:46 +0000

    Checkpoint

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const multer  = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ dest: 'uploads/' })
 const router = express.Router();
 const path = require('path');
 
@@ -10,10 +10,12 @@ router.get("/", function (request, response) {
   response.sendFile(path.join('/app/views/index.html'));
 });
 
-router.post('/file',upload.single('avatar'), (req,res)=>{
-    console.log('body: ',req.body);
-    console.log('files: ',req.files);
+ 
+router.post('/file', upload.single('file'), function (req, res, next) {
+  console.log('body: ',req.body);
+  console.log('files: ',req.files);
   res.end();
-});
+  // req.body contains the text fields 
+})
 
 module.exports = router;
